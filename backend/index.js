e9e9a2d93ea40fb1815b8452e8f02a3b05816a8a
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-07-30 13:26:37 -0400

    added a post route

--- backend/index.js
+++ backend/index.js
@@ -14,10 +14,22 @@ const port = 3000;
 // allow requests from frontend
 app.use(cors());
 
+// format incoming data to json
+app.use(express.json());
+
+// GET todos
 app.get('/api/todos', async (req, res) => {
     const todos = await Todo.find({});
     res.json(todos);
 })
+
+// POST create a todo
+app.post('/api/todos', async (req, res) => {
+    console.log(req.body);
+    const todo = await Todo.create(req.body);
+    res.json(todo);
+})
+
 
 app.listen(port, () => {
     console.log('Listening on port: ', port);
