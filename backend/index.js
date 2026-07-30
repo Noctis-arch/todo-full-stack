23a71e5f3fa1e0bc9664329bb1942ac6b4c7ef72
marlonrod12@gmail.com <247076703+Noctis-arch@users.noreply.github.com>
2026-07-30 09:57:30 -0400

    finished test function

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello (from server)');
-});
+app.get('/test', (req, res) => {
+    res.send('Hello (from server)')
+})
 
 app.listen(port, () => {
-    console.log('Listening on port:', port);
-});
+    console.log('Listening on port: ', port);
+})
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,18 @@ import { useEffect } from "react"
 export default function App() {
 
   async function test() {
-      fetch();
+    const response = await fetch('http://localhost:3000/test');
+    const data = await response.json();
+    console.log(data);
   }
 
   useEffect(() => {
-
+    test();
   }, []);
 
   return (
     <div>
       Hello World!
     </div>
-
-
   )
-
 }
